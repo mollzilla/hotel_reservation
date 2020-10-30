ea3c9a7f0b1ad26829f233cbba089dd6cfbf0fc3
mollzilla <eymili@gmail.com>
2020-10-29 21:27:25 -0300

    fechas funcionan

--- Header.js
+++ Header.js
@@ -1,20 +1,31 @@
 const { FormControl, InputLabel, MenuItem, Select, TextField, Box, Card, makeStyles, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography, Icon, Paper, Grid } = window['MaterialUI'];
 
 
-
-
-
 class Header extends React.Component {
 
 
   render() {
 
+    const {
+      handleDateChange,
+      since,
+      until
+    } = this.props;
 
+    const daysOfWeek = ["Lunes", "Martes", "Miércoles", "Jueves", "Sábado", "Domingo"];
+    const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    const getDateString = (limit) => {
+      console.log("mili")
+    return  limit ? ` el ${daysOfWeek[limit.getDay()]} ${since.getDate()} ${months[limit.getMonth()]} de ${limit.getFullYear()}`: " hoy"
+    }
+
+    console.log(typeof since)
     return (
       <header>
         <Box className="header" color="white" bgcolor="success.main">
           <h1>Hoteles</h1>
-          <h3>Hola paper 2</h3>
+        <h3>Desde{getDateString(since)} hasta {getDateString(until)}</h3>
         </Box>
         <Box className="params-header" color="white" bgcolor="primary.main">
           <form className="params-form" noValidate>
@@ -25,15 +36,15 @@ class Header extends React.Component {
               label="Desde"
               type="date"
               defaultValue="2017-05-24" // hacerlo con now
-              // className={classes.textField}
+              className={"date-picker"}
               InputLabelProps={{
                 shrink: true,
               }}
 
               format="dd/MM/yyyy"
               margin="dense"
-            // value={selectedDate}
-            // onChange={handleDateChange}
+              // value={selectedDate}
+              onChange={(e) => {handleDateChange("since", e.target.value)}}
             />
 
             <TextField
@@ -41,15 +52,16 @@ class Header extends React.Component {
               label="Hasta"
               type="date"
               defaultValue="2017-05-24" // hacerlo con now
-              // className={classes.textField}
+              className={"date-picker"}
               InputLabelProps={{
                 shrink: true,
+                // className: "date-picker"
               }}
 
               format="dd/MM/yyyy"
               margin="dense"
             // value={selectedDate}
-            // onChange={handleDateChange}
+              onChange={(e) => {handleDateChange(e.target.id, e.target.value)}}
             />
 
             <FormControl className="country-picker" margin="dense">
@@ -58,7 +70,7 @@ class Header extends React.Component {
                 margin="dense"
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                placeHolder="Todos los Paises"
+                placeholder="Todos los Paises"
               // onChange={handleChange}
               >
                 <MenuItem value={10}>Argentina</MenuItem>
--- HotelContainer.js
+++ HotelContainer.js
@@ -4,20 +4,47 @@ class HotelContainer extends React.Component {
   /// hotel Container recibe como props data y al ser el padre es la unica fuente de la verdad, de ahi la pasa al header y al grid
 
   state = {
-    cosas: "the single source of truth"
+    cosas: "the single source of truth",
+    since: null,
+    until: null,
+    country: null
   }
+
+  handleDateChange=(limit, value) => {
+
+    console.log(limit, value)
+
+    this.setState({
+      ...this.state,
+      [limit]: new Date(value),
+    });
+    console.log(this.state)
+  };
+
+  handleCountryChange=(country) => {
+    this.setState({
+      country: country
+    })
+  }
+
+  
+
 
   render() {
 
     // console.log(this.props.data)
     return (
       <Container>
-        <Header />
+        <Header
+          handleDateChange={this.handleDateChange}
+          since={this.state.since}
+          until={this.state.until}
+        />
 
         <Grid container spacing={8}>
 
           {this.props.data.map((hotel, i) => (
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={4} key={i}>
               <Hotelcard
                 hotel={hotel}
                 key={i}
